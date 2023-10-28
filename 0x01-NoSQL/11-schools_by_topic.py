f6975efbd4bbd99ff17c0e43e4d620f2664f9041
EphraimShikanga <ephraimshikanga@gmail.com>
2023-10-28 17:09:03 +0300

    Mongo_Python

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+def schools_by_topic(mongo_collection, topic):
+    """
+    Retrieve the list of schools in a PyMongo collection
+    based on a specific topic.
+
+    Parameters:
+        mongo_collection (pymongo.collection.Collection):
+        The PyMongo collection object.
+        topic (str): The topic to search for.
+
+    Returns:
+        list: A list of schools matching the specified
+        topic.
+    """
+    schools = list(mongo_collection.find({"topic": topic}))
+
+    return schools
